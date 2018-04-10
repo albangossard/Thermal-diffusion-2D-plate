a2d5a4c9e38586dc6b85363d7085420a6bce8c5f
Alban Gossard <alban.gossard@hotmail.fr>
2018-04-10 18:13:29 +0200

    ready to test

--- courbeConvergence1.py
+++ courbeConvergence1.py
@@ -1,5 +1,6 @@
 from fem2d import *
-from sklearn import linear_model
+if not os.path.isfile("NOPLOT"):
+	from sklearn import linear_model
 
 # list_N=[6,10,20,30,50,80,100]
 # list_N=[6,10,20,30,50]
@@ -50,32 +51,36 @@ for N in list_N:
         list_errL2.append(np.linalg.norm(diff)/(diff.shape[0]*diff.shape[1]))
         list_errH1.append(np.linalg.norm(diff)/(diff.shape[0]*diff.shape[1])+np.linalg.norm(np.gradient(diff))/(diff.shape[0]*diff.shape[1]))
 
-list_NRegr=np.log(np.array(list_N)).reshape((-1,1))
-list_errL2Regr=np.log(np.array(list_errL2)).reshape((-1,1))
-list_errH1Regr=np.log(np.array(list_errH1)).reshape((-1,1))
-print("L2")
-regrL2 = linear_model.LinearRegression()
-regrL2.fit(list_NRegr, list_errL2Regr)
-print('slope', regrL2.coef_)
-print('intercept', regrL2.intercept_)
-print('score', regrL2.score(list_NRegr, list_errL2Regr))
-print("H1")
-regrH1 = linear_model.LinearRegression()
-regrH1.fit(list_NRegr, list_errH1Regr)
-print('slope', regrH1.coef_)
-print('intercept', regrH1.intercept_)
-print('score', regrH1.score(list_NRegr, list_errH1Regr))
+np.savetxt('courbeCV1_list_N.txt',list_N)
+np.savetxt('courbeCV1_list_errL2.txt',list_errL2)
+np.savetxt('courbeCV1_list_errH1.txt',list_errH1)
+if not os.path.isfile("NOPLOT"):
+    list_NRegr=np.log(np.array(list_N)).reshape((-1,1))
+    list_errL2Regr=np.log(np.array(list_errL2)).reshape((-1,1))
+    list_errH1Regr=np.log(np.array(list_errH1)).reshape((-1,1))
+    print("L2")
+    regrL2 = linear_model.LinearRegression()
+    regrL2.fit(list_NRegr, list_errL2Regr)
+    print('slope', regrL2.coef_)
+    print('intercept', regrL2.intercept_)
+    print('score', regrL2.score(list_NRegr, list_errL2Regr))
+    print("H1")
+    regrH1 = linear_model.LinearRegression()
+    regrH1.fit(list_NRegr, list_errH1Regr)
+    print('slope', regrH1.coef_)
+    print('intercept', regrH1.intercept_)
+    print('score', regrH1.score(list_NRegr, list_errH1Regr))
 
 
 
-# fig = plt.figure()
-ax = plt.gca()
-ax.plot(list_N, list_errL2, 'o', label='L2', markeredgecolor='none')
-ax.plot(list_N, list_errH1, 'o', label='H1', markeredgecolor='none')
-ax.set_yscale('log')
-ax.set_xscale('log')
-plt.grid(True, which="both")
-plt.xlabel('N')
-plt.ylabel('Error')
-plt.legend()
-plt.show()
+    # fig = plt.figure()
+    ax = plt.gca()
+    ax.plot(list_N, list_errL2, 'o', label='L2', markeredgecolor='none')
+    ax.plot(list_N, list_errH1, 'o', label='H1', markeredgecolor='none')
+    ax.set_yscale('log')
+    ax.set_xscale('log')
+    plt.grid(True, which="both")
+    plt.xlabel('N')
+    plt.ylabel('Error')
+    plt.legend()
+    plt.show()
