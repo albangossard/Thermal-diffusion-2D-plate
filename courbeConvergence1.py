a28196e83e66d327e1cd4bc9983a7afda4e93d97
Alban Gossard <alban.gossard@hotmail.fr>
2018-04-10 18:14:14 +0200

    calcul plus gros

--- courbeConvergence1.py
+++ courbeConvergence1.py
@@ -2,9 +2,9 @@ from fem2d import *
 if not os.path.isfile("NOPLOT"):
 	from sklearn import linear_model
 
-# list_N=[6,10,20,30,50,80,100]
+list_N=[6,10,20,30,50,80,100]
 # list_N=[6,10,20,30,50]
-list_N=np.arange(2,21,2)
+# list_N=np.arange(2,21,2)
 
 exactNorm = False
 
